59c7fcd3f9a7adefd5e866afeaa126f7347d4e3c
DESKTOP-UCH5P2T\Sonia Layra E Y <sonialayra15@gmail.com>
2024-01-14 12:40:11 +0700

    no message

---
+++ resources/views/index.blade.php
@@ -0,0 +1,346 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>The Wedding of Mustika & Wira - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/Leaf_1.png" rel="icon">
+  <link href="assets/img/Leaf_1.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-fv9+3vSe35omh6M15z7tPrWCHJdY3tPib/zPV6cFo5AxD7cfI68YqZR5F+7a6GpfqlqACzyDokxt8YGnYpCfAA==" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <style>
+        @keyframes zoomInOutAnimation {
+            0%, 100% {
+                transform: scale(0.8);
+            }
+            50% {
+                transform: scale(0.9);
+            }
+        }
+
+
+        #myAudio {
+      display: none;
+    }
+
+    #playPauseButtonContainer {
+      position: absolute;
+      top: 700px;
+      right: 10px;
+    }
+
+    #playPauseIcon {
+      
+      cursor: pointer;
+      color: #5c6957;
+    }
+
+  #text {
+    transform: rotate(315deg);
+  }
+
+  #text span {
+    font-size: 40px;
+    font-weight: 600;
+    line-height: 60px;
+    height: 250px;
+    position: absolute;
+    font-family: "Times New Roman";
+    top: -110px;
+    left: 140px;
+    transform-origin: 50% 100%;
+    color: #5c6957;
+    
+  }
+  #t1 {transform: rotate(6deg);
+  }
+  #t2 {transform: rotate(12deg);
+  }
+  #t3 {transform: rotate(18deg);
+  }
+  #t4 {transform: rotate(24deg);
+  }
+  #t5 {transform: rotate(26deg);
+  }
+  #t6 {transform: rotate(36deg);
+  }
+  #t7 {transform: rotate(42deg);
+  }
+  #t8 {transform: rotate(48deg);
+  }
+  #t9 {transform: rotate(54deg);
+  }
+  #t10 {transform: rotate(60deg);
+  }
+  #t11 {transform: rotate(66deg);
+  }
+  #t12 {transform: rotate(72deg);
+  }
+  #t13 {transform: rotate(78deg);
+  }
+  #t14 {transform: rotate(84deg);
+  }
+  </style>
+</head>
+
+<body>
+<main id="main">
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero">
+    <div class="container" data-aos="zoom-in">
+
+    <audio id="myAudio" controls autoplay>
+  <source src="assets/img/Beautiful_In_White.mp3" type="audio/mp3">
+  Your browser does not support the audio element.
+</audio>
+
+<div id="playPauseButtonContainer">
+  <button onclick="togglePlayPause()">
+    <i id="playPauseIcon" class="fas fa-play"></i>
+  </button>
+</div>
+
+    <center>
+    <div class="row">
+    <div class="col-xl-12">
+        <div id="text">
+        <span id="t1">T</span>
+        <span id="t2">h</span>
+        <span id="t3">e</span>
+        <span id="t4"></span>
+        <span id="t5">W</span>
+        <span id="t6">e</span>
+        <span id="t7">d</span>
+        <span id="t8">d</span>
+        <span id="t9">i</span>
+        <span id="t10">n</span>
+        <span id="t11">g</span>
+        <span id="t12"></span>
+        <span id="t13">o</span>
+        <span id="t14">f</span>
+        </div>
+          <div class="portfolio-wrap">
+              <img src="assets/img/Photo_Profil.png" class="img-fluid" alt="" 
+              style="border-radius: 500px; border: 3px solid #adb4a6; padding: 5px;
+                    height: 550px; width: 300px; 
+                    ">
+          </div> <br>
+          <h2 style="font-size: 80px; animation: zoomInOutAnimation 5s ease-in-out infinite;">Mustika </h2>
+          <h2 style="font-size: 80px; animation: zoomInOutAnimation 5s ease-in-out infinite;"> & Wira</h2>
+          <div class="icon-box">
+            <h3>25 | 05 | 2022</h3>
+          </div>
+        <h6 style="padding: 15px; font-size: 20px; font-family: Times New Roman; color: #5c6957;">di mohon kehadiranya</h6>
+      </div>
+    </div>
+    </div>
+   </center>
+
+    <div class="row">
+    <div class="col-lg-12">
+
+    <div class="portfolio-wrap">
+        <img src="assets/img/Flower_3.png" class="img-fluid" alt="" 
+        style="position: absolute; top: -150px; right: -180px; 
+        transform: translate(100%, 100%); 
+        animation: zoomInOutAnimation 5s ease-in-out infinite;
+        height: 500px; width: 500px; rotate: -10deg;
+        ">
+    </div>
+
+    <div class="portfolio-wrap">
+        <img src="assets/img/Flower_4.png" class="img-fluid" alt="" 
+        style="position: absolute; bottom: -50px; right: -280px; 
+        transform: translate(100%, 100%); animation: zoomInOutAnimation 5s ease-in-out infinite;
+        height: 500px; width: 500px; rotate: 330deg;
+        ">
+    </div>
+
+    <div class="portfolio-wrap">
+        <img src="assets/img/Flower_1.png" class="img-fluid" alt="" 
+        style="position: absolute; bottom: -280px; left: -115px;
+        transform: translate(100%, 100%); animation: zoomInOutAnimation 5s ease-in-out infinite;
+        height: 500px; width: 500px; rotate: 0deg;
+        ">
+    </div>
+
+    <div class="portfolio-wrap">
+        <img src="assets/img/Leaf_2.png" class="img-fluid" alt="" 
+        style="position: absolute; bottom: 450px; left: 50px;
+        transform: translate(100%, 100%); animation: zoomInOutAnimation 5s ease-in-out infinite;
+        height: 100px; width: 100px;
+        ">
+    </div>
+
+    <div class="portfolio-wrap">
+        <img src="assets/img/Leaf_1.png" class="img-fluid" alt="" 
+        style="position: absolute; bottom: 380px; right: 50px;
+        transform: translate(100%, 100%); animation: zoomInOutAnimation 5s ease-in-out infinite;
+        height: 100px; width: 100px;
+        ">
+    </div>
+
+    </div>
+    </div>
+
+    </div>
+  </section><!-- End Hero -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+    <div class="container" data-aos="zoom-in">
+
+    <div class="row">
+      <div class="col-lg-2">
+      <div class="portfolio-wrap">
+              <img src="assets/img/Flower_1.png" class="img-fluid" alt="" 
+              style="position: absolute; top: -280px; left: -115px; 
+              transform: translate(-100%, -100%); animation: zoomInOutAnimation 5s ease-in-out infinite;
+              height: 500px; width: 500px; rotate: 0deg;
+              ">
+            </div>
+      </div>
+
+      <div class="row">
+      <div class="col-xl-12">
+    <center>
+      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+        <div class="col-lg-12" style="padding: 100px; margin: 100px;">
+          <h1 style="color: #b28c44;"><i>Assalamualaikum Wr. Wb.</i></h1>
+         
+          <h2 style="padding-bottom: 30px;">Tanpa Mengurangi rasa hormat, Kami mengundang <br>
+              Bapak/Ibu/Saudara/i pada acara pernikahan kami:</h2>
+
+          <h1 style="color: #869580; font-size: 50px; animation: zoomInOutAnimation 5s ease-in-out infinite; font-weight: 500;">Mustika</h1>
+          <h2>Putri dari Bpk Winarno & Ibu Julaikah</h2>
+          <h1 style="color: #869580; font-size: 50px; font-weight: 500;">&</h1>
+          <h1 style="color: #869580; font-size: 50px; animation: zoomInOutAnimation 5s ease-in-out infinite; font-weight: 500;">Wira</h1>
+          <h2 style="padding-bottom: 20px;">Putra dari Bpk Winarno & Ibu Julaikah</h2>
+
+          <h1 style="color: #b28c44;"><i>QS. Ar-Rum Ayat 21</i></h1>
+          <h2>
+            Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia
+            menciptakan pasangan-pasangan untukmu dari jenismu sendiri,
+            agar kamu cenderung dan merasa tenteram kepadanya, dan Dia
+            menjadikan di antaramu rasa kasih sayang. Sungguh, pada
+            yang demikian itu benar-bener terdapat tanda-tanda (Kebesaran 
+            Allah) bagi kaum yang berfikir.
+          </h2>
+        </div>
+      </div>
+     
+    </center>
+
+    </div>
+    </div>
+
+    
+    <div class="row">
+    <div class="col-lg-2">
+      <div class="portfolio-wrap">
+              <img src="assets/img/Flower_2.png" class="img-fluid" alt="" 
+              style="position: absolute; bottom: -500px; left: 20px;
+              transform: translate(-100%, -100%); animation: zoomInOutAnimation 5s ease-in-out infinite;
+              height: 700px; width: 1500px; rotate: 0deg;
+              ">
+            </div>
+
+    <div class="portfolio-wrap">
+        <img src="assets/img/Leaf_1.png" class="img-fluid" alt="" 
+        style="position: absolute; bottom: 350px; left: 50px;
+        transform: translate(100%, 100%); animation: zoomInOutAnimation 5s ease-in-out infinite;
+        height: 100px; width: 100px;
+        ">
+    </div>
+
+    <div class="portfolio-wrap">
+        <img src="assets/img/Leaf_2.png" class="img-fluid" alt="" 
+        style="position: absolute; bottom: 650px; right: 50px;
+        transform: translate(100%, 100%); animation: zoomInOutAnimation 5s ease-in-out infinite;
+        height: 100px; width: 100px;
+        ">
+    </div>
+    
+      </div>
+      </div>
+
+    </div>
+    </section><!-- End About Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by Sonia Layra Ega Yanti
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+  <script>
+  var audio = document.getElementById("myAudio");
+  var playPauseButton = document.getElementById("playPauseIcon");
+
+  audio.addEventListener('play', function () {
+    playPauseButton.className = "fas fa-pause";
+  });
+
+  audio.addEventListener('pause', function () {
+    playPauseButton.className = "fas fa-play";
+  });
+
+  function togglePlayPause() {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
+</script>
+
+</body>
+
+</html>
